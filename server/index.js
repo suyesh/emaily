ea82fd41a76c8861d2f7b09c999f0c7d249c7aee
Suyesh Bhandari <suyeshb@gmail.com>
2018-07-31 19:18:34 -0400

    Auth flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const googleKeys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({ test: 'test' });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: googleKeys.googleClientId,
+			clientSecret: googleKeys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log('accessToken:', accessToken);
+			console.log('refreshToken:', refreshToken);
+			console.log('profile:', profile);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
